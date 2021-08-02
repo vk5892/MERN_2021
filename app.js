8aafeb9893ab50f1cca5150789234d4788a7cd44
Vikas Khade <khadevikas59@gmail.com>
2021-08-02 09:15:27 +0530

    json basics

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log('hi')
 console.log('first program')
+const friends = ['vikas','pradeep'];
+friends.map(name => console.log(name))
+
+for(i = 0; i <=friends.length, i++) {
+    console.log(friends[i]);
+    }
